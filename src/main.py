d71c45f861b4956bc662554e9e3522cc52655d80
Jakob <jakob.symchych@my.utsa.edu>
2025-06-04 13:23:31 -0500

    init main

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 import logging
+import uvicorn
 from config.logging_config import config_logging
+from fastapi import FastAPI
+
 
 # init logging
 config_logging()
 logger = logging.getLogger('api')
 
-def main():
-    pass
+app = FastAPI(title="hltkn_api", description="API for timescaleDB", version=-"0.0.1")
 
+@app.get("/health")
+async def health_check():
+    return {"status": "healthy"}
 
 if __name__ == "__main__":
-    main()
-
-
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
