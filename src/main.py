f728d4aa9a32ab3d48c4741ab702afe27f37110b
jakobsym <jakob.symchych@my.utsa.edu>
2025-06-06 10:56:20 -0500

    add init of routers

--- src/main.py
+++ src/main.py
@@ -2,12 +2,16 @@ import logging
 import uvicorn
 from config.logging_config import config_logging
 from fastapi import FastAPI
+from api.protocol_routes import router as protocols_router
+from api.token_routes import router as tokens_router
 
 # init logging
 config_logging()
 logger = logging.getLogger('api')
 
 app = FastAPI(title="hltkn_api", description="API for timescaleDB", version="0.0.1")
+app.include_router(protocols_router, prefix="/protocols", tags=["protocols"])
+app.include(tokens_router, prefix="/tokens", tags=["tokens"])
 
 @app.get("/health")
 async def health_check():
